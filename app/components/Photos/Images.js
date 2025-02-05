95e3454acb42c962a4af3c66406c627f48a3af60
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2025-02-05 13:17:20 +0200

    Update Images.js

--- app/components/Photos/Images.js
+++ app/components/Photos/Images.js
@@ -4,6 +4,13 @@ import photo3 from "../../../public/photo-3.jpg";
 import photo4 from "../../../public/photo-4.jpg";
 import photo5 from "../../../public/photo-5.jpg";
 import photo6 from "../../../public/photo-6.jpg";
+import photo7 from "../../../public/photo-7.jpg";
+import photo8 from "../../../public/photo-8.jpg";
+import photo9 from "../../../public/photo-9.jpg";
+import photo10 from "../../../public/photo-10.jpg";
+import photo11 from "../../../public/photo-11.jpg";
+import photo12 from "../../../public/photo-12.jpg";
+import photo13 from "../../../public/photo-13.jpg";
 
 export const images = [
   { image: photo1, alt: "Truck" },
@@ -12,4 +19,11 @@ export const images = [
   { image: photo4, alt: "Truck" },
   { image: photo5, alt: "Truck" },
   { image: photo6, alt: "Truck" },
+    { image: photo7, alt: "Truck" },
+  { image: photo8, alt: "Truck" },
+  { image: photo9, alt: "Truck" },
+  { image: photo10, alt: "Truck" },
+  { image: photo11, alt: "Truck" },
+  { image: photo12, alt: "Truck" },
+    { image: photo13, alt: "Truck" },
 ];
